6d8fc076ed228162686cb84e1b7bbad25affb0bf
rsushant583 <110271416+rsushant583@users.noreply.github.com>
2022-10-21 18:23:48 +0700

    Add files via upload
    these are the solutions of assingment1 problems

---
+++ q3.c
@@ -0,0 +1,7 @@
+/*3. Write a
+program to print “iNeuron” on the screen. (Remember to print in double quotes)*/
+#include<stdio.h>
+int main()
+{
+    printf("\"iNeuron"");
+}
---
+++ question practice.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+int main()
+{
+   /* int x=printf("ineuron");    //question 12 assingment 1
+    printf("%d \n",x);
+    getchar();*/
+
+
+   /* int x,y;                                       //question 11 assingment 1
+    printf("Enter the time in HH:MM formate \n");
+    scanf("%d:%d",&x,&y);
+    printf("%d hour and %d Minute",x,y);
+    getchar();
+    return 0;*/
+
+
+    /*int x,y,z;                       // question 10 assingment 1
+    printf("Enter a date (such as your DOB) in DD/MM/YYYY formate ");
+    scanf("%d/%d/%d",&x,&y,&z);
+    printf("Day-%d , Month-%d , Year-%d",x,y,z);*/
+
+
+  //  printf("\\\\");             question 9 assingment 1
+
+
+ // printf("\\n");               question 8 assingment 1
+
+
+  //printf("%%d");                 question 7 assingment 1
+
+
+
+ /* char name[20];                question 6 assingment 1
+  printf("Enter your name \n");
+  scanf("%s",name);
+  printf("   \"Hello %s \"  " ,name);
+  getchar();*/
+
+
+
+  /* float a,r;                                          question 4 assingment 1
+   printf("Enter radius of the circle");
+   scanf("%f",&r);
+   a=3.14*r*r;
+   printf("Area of the circle with radius %f is %.2f",r,a);
+   getch();*/
+
+
+//   printf(" \"iNeuron\" ");                        question 3 assingment 1
+
+  // printf("Hello \n student");                     question 2 assingment 1
+
+
+     printf("Hello student");
+
+
+}
